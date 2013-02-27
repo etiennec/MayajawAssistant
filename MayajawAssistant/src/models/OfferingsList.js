1c1300529e2cd4683a04a1dc197f927dfd423371
etiennec <etienne.canaud@hp.com>
2013-02-27 15:04:02 +0800

    work in progress

---
+++ MayajawAssistant/src/models/OfferingsList.js
@@ -0,0 +1,162 @@
+var OfferingsList = {
+    monthlyOfferings: [], // Array of monthly offerings
+    toString: function () {
+        var string = "Total count:" + this.monthlyOfferings.length;
+        for (var i = 0; i < this.monthlyOfferings.length; i++) {
+            var monthlyOffering = this.monthlyOfferings[i];
+            string += "\n" + monthlyOffering.toString();
+        }
+
+        return string;
+    },
+
+    toObject: function () {
+        return {
+            Offerers: this.getOfferersList(null),
+            Offerees: this.getOffereesList(null)
+        }
+    },
+
+    getOfferersList: function (offereeFilterName) {
+
+        var offerersSet = [];
+        var offerersTotal = {};
+        var offerings = this.getOfferingsList(null, null);
+
+        for (var i = 0; i < offerings.length; i++) {
+            var offering = offerings[i];
+            var to = offering.get("to");
+            if (offereeFilterName != null && to != offereeFilterName) {
+                continue;
+            }
+
+            var from = offering.get("from");
+            var of = offering.get("of");
+            if (offerersSet.indexOf(from) == -1) {
+                // New offerer
+                offerersSet.push(from);
+                offerersTotal[from] = of;
+            } else {
+                // Existing offerer
+                offerersTotal[from] += of;
+            }
+        }
+
+        var offerersList = [];
+        for (var i = 0; i < offerersSet.length; i++) {
+            var name = offerersSet[i];
+            offerersList.push({Name: name, Value: offerersTotal[name]});
+        }
+
+        // We sort the array from the biggest contributor to the smallest
+        offerersList.sort(function (a, b) {
+            return b.Value - a.Value;
+        })
+
+        return offerersList;
+    },
+
+    getOffereesList: function (offererFilterName) {
+
+        var offereesSet = [];
+        var offereesTotal = {};
+        var offerings = this.getOfferingsList(null, null);
+
+        for (var i = 0; i < offerings.length; i++) {
+            var offering = offerings[i];
+            var from = offering.get("from");
+            if (offererFilterName != null && from != offererFilterName) {
+                continue;
+            }
+
+            var to = offering.get("to");
+            var of = offering.get("of");
+            if (offereesSet.indexOf(to) == -1) {
+                // New offerer
+                offereesSet.push(to);
+                offereesTotal[to] = of;
+            } else {
+                // Existing offerer
+                offereesTotal[to] += of;
+            }
+        }
+
+        var offereesList = [];
+        for (var i = 0; i < offereesSet.length; i++) {
+            var name = offereesSet[i];
+            offereesList.push({Name: name, Value: offereesTotal[name]});
+        }
+
+        // We sort the array from the biggest contributor to the smallest
+        offereesList.sort(function (a, b) {
+            return b.Value - a.Value;
+        })
+
+        return offereesList;
+    },
+
+    // Return the list of all offerings. Can pass optional year and/or month to filter data returned.
+    getOfferingsList: function (year, month) {
+        var offerings = [];
+
+        for (var i = 0; i < this.monthlyOfferings.length; i++) {
+            var monthlyOffering = this.monthlyOfferings[i];
+            if (year != null && monthlyOffering.get("year") != year) {
+                continue;
+            }
+            if (month != null && monthlyOffering.get("month") != month) {
+                continue;
+            }
+
+            for (var j = 0; j < monthlyOffering.get("offerings").length; j++) {
+                offerings.push((monthlyOffering.get("offerings")[j]));
+            }
+        }
+
+        return offerings;
+    },
+
+    // Return the contents ready for use in a stacked Area chart or a stacked columns chart.
+    getAsStackedData: function () {
+        var categories = [];
+        // This var will contain arrays of series with offerer as index.
+        var tmpSeries = {};
+
+        for (var i = 0; i < this.monthlyOfferings.length; i++) {
+            var monthlyOffering = this.monthlyOfferings[i];
+            categories.push(monthlyOffering.getDateAsString());
+            for (var j = 0; j < monthlyOffering.get("offerings").length; j++) {
+                var offering = monthlyOffering.get("offerings")[j];
+                var offerer = offering.get("from");
+                if (tmpSeries[offerer] === undefined) {
+                    tmpSeries[offerer] = [];
+                }
+                if (tmpSeries[offerer][i] === undefined) {
+                    tmpSeries[offerer][i] = 0;
+                }
+                tmpSeries[offerer][i] += offering.get("of");
+            }
+        }
+
+        // now we convert the temporary series to the real series object
+        var series = [];
+
+        for (var key in tmpSeries) {
+            var serieWithoutMissingValues = tmpSeries[key];
+
+            for (var i = 0; i < categories.length; i++) {
+                if (serieWithoutMissingValues[i] === undefined) {
+                    serieWithoutMissingValues[i] = 0;
+                }
+            }
+
+            series.push({name: key, data: serieWithoutMissingValues, color: ChartsUtils.getColor(key)});
+        }
+
+        return {
+            categories: categories,
+            series: series
+        }
+
+    }
+};
