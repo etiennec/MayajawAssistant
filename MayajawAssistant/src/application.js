fbfb77b01eb5a16fb80855c6da4740ac08266bff
etiennec <etienne.canaud@hp.com>
2013-02-28 16:16:13 +0800

    removing unused files

--- MayajawAssistant/src/application.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    $("#subtitle").append(MSUtils.getRandomQuote())
-    var app = new AppRouter();
-    Backbone.history.start();
-
-    //$("#cenote-input-text").val($("#cenote-sample-tmpl").html())
-    //$("#market-input-text").val($("#market-sample-tmpl").html())
-
-})(jQuery);
